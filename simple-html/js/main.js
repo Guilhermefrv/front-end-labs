06b6955c4a32d0bd4739ec49a2b502b4e1185d10
Guilhermefrv <romaoguilherme557@gmail.com>
2025-05-08 22:22:18 -0300

    testando_javascript

--- simple-html/js/main.js
+++ simple-html/js/main.js
@@ -1,2 +1,27 @@
-let nome = "Guilherme";
-alert("Boa noite, " + nome);
+const calc = (a, b, operacao) => {
+    return operacao === "+" ? a + b
+         : operacao === "-" ? a - b
+         : operacao === "*" ? a * b
+         : operacao === "/" ? a / b
+         : "Operação Inválida";
+};
+
+const calcCase = (a, b, operacao) => {
+    if (b === 0 && operacao === "/") {
+        return "Não há divisão por zero!";
+    }
+    else{
+    switch(operacao) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return a / b;
+        default:
+            return "Operação Inválida";
+        }
+    }    
+};
