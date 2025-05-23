e2cb5e555017844f21358161d1a58ab528fa2cb4
Guilhermefrv <romaoguilherme557@gmail.com>
2025-05-22 22:23:31 -0300

    Virtual Phone part 2

---
+++ main.js
@@ -0,0 +1,26 @@
+const display = document.querySelector ('input[type=tel]');
+
+const teclas = document.querySelectorAll ('input[type=button]');
+
+const limpar = document.querySelector('.Icon');
+
+limpar.onclick = () => {
+    const opcao = confirm('Deseja mesmo limpar?')
+    if(opcao) {
+        display.value = "";
+    }
+    
+}
+
+function valorTeclaToDisplay (i) {
+    display.value += teclas[i].value;
+}
+
+//Criar uma condição para travar mais 14 digitos
+for(let i = 0; i < teclas.length; i++){
+    teclas[i].onclick = () => {
+        valorTeclaToDisplay(i);
+    }
+}
+
+
